b394c5af2b88d921bf7d712bc263a9519c96130d
JohnMigwi <johnmigwi101@gmail.com>
2023-09-15 13:23:24 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div className='App'>
+      <div className='searchBarContainer'>
+        <div>SearchBar</div>
+        <div>QueryResults</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
